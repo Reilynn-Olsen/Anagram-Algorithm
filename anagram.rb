de0d33d07327a13222bdbc330495ad2d0801716c
Reilynn <rieolsen00@gmail.com>
2021-10-24 11:53:45 -0600

    Commit

---
+++ anagram.rb
@@ -0,0 +1,19 @@
+class Anagram
+  attr_reader :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    matches = []
+    array.each do |w|
+      matches.push(w) if matches?(w.downcase)
+    end
+    matches
+  end
+
+  def matches?(w)
+    w.chars.sort == word.downcase.chars.sort && w != word.downcase
+  end
+end
